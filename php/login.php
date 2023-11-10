8b665740ddddf1933683a49bf6618fa21bb9c542
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-10 16:13:44 -0300

    fixed formatting

--- php/login.php
+++ php/login.php
@@ -37,28 +37,31 @@ mysqli_close($conn);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
 
-    <h1>Login</h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
 
-    <?php if (isset($error_message)): ?>
+    <body>
+
+        <h1>Login</h1>
+
+        <?php if (isset($error_message)): ?>
         <p style="color: red;"><?php echo $error_message; ?></p>
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <form action="login.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
+        <form action="login.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" name="email" required>
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" required>
+            <label for="password">Senha:</label>
+            <input type="password" name="password" required>
 
-        <button type="submit">Entrar</button>
-    </form>
+            <button type="submit">Entrar</button>
+        </form>
 
-</body>
+    </body>
+
 </html>
