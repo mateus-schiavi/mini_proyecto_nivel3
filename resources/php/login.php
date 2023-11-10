7b8a1846ca5978742b88e8daeb7918801cc8678b
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-09 22:35:30 -0300

    missing css and js

--- resources/php/login.php
+++ resources/php/login.php
@@ -2,7 +2,7 @@
  session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require './database/login_db.php';
+    require '../../database/login_db.php';
 
     $email = $_POST['email'];
     $password = $_POST['password'];
