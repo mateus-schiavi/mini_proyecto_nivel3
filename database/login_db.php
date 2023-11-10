388c5e2446947f87fc2cdfe4c031bd5e5c211db9
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-10 15:20:05 -0300

    changed host

--- database/login_db.php
+++ database/login_db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
+$host = "localhost";
 $username = "root";
 $password = "";
 $database = "login_db";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
 }
