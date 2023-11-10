fd9e787a43ba93b3c860097fe9d2828bf5ea3738
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-10 15:19:15 -0300

    deleted files

--- database/login_db.php
+++ database/login_db.php
@@ -1,12 +1,15 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'login_db';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "login_db";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection Error: " . $e->getMessage());
+    echo "Connected successfully"; 
 }
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
